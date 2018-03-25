66d8828bc5787b13b0a2001598583422cf8c478a
songjun51 <song@wonjun.kr>
2018-03-25 23:47:02 +0900

    y

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    
+</div>
+</template>
+
+<script>
+export default {
+  name: "#app",
+   data() {
+    //데이터는 정적인 데이터 처리해
+    return {
+      msg : '15회 앱잼은 현재'
+    };
+  },
+  computed: {
+    // 계산 필요한 동적 데이터 처리
+  },
+  methods: {
+    // 메소드 ()
+  },
+  watch: {
+    // 뒷편에서 이벤트 잡을려고
+  }
+};
+//나누는 이유는 뷰 규칙, 데이터랑 컴퓨트랑 나누는 이유는 연산 등 내부 로직자체가 달라서 퍼포먼스때문에
+</script>
+
+<style>
+@import "assets/app.css";
+</style>
